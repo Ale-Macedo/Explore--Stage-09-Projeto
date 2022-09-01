a1286f0ed3ec3a3f232d029561001c8d43a31817
Alessandro Macedo <aleluiz73@gmail.com>
2022-09-01 09:46:52 -0300

    adicionando componentes globais
    -criando padrões css globais
    - intendendo como funciona um componente
    -criando componente Button

--- Rocketnotes/src/pages/Details/index.jsx
+++ Rocketnotes/src/pages/Details/index.jsx
@@ -1,11 +1,17 @@
 import { Container } from './styles';
 
+import { Button } from '../../components/Button';
+
 export function Details() {
   
   return(
     <Container>
     <h1>koe koe</h1>
     <samp>menó</samp>
+
+    <Button title="Aperta o meu botão" loading  />
+    <Button title="koe o meu bortao não" />
+    <Button title="koe koe" />
     </Container>
   )
 }
--- Rocketnotes/src/pages/Details/styles.js
+++ Rocketnotes/src/pages/Details/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background: rgb(126, 224, 195);
+  /* background: rgb(126, 224, 195); */
 `
 
 
