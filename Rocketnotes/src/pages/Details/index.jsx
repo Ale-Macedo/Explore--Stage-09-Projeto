2de1b7bdc43c219f934dde267c9201d6f56a4c29
Alessandro Macedo <aleluiz73@gmail.com>
2022-08-31 22:02:23 -0300

    css-in-js
    - compreendendo o que é e como funciona o css-in-js
    - adicionando a biblioteca styled-components
    - adicionando tabela de cores para o projeto

--- Rocketnotes/src/pages/Details/index.jsx
+++ Rocketnotes/src/pages/Details/index.jsx
@@ -1,11 +1,11 @@
-import "./styles.css"
+import { Container } from './styles';
 
 export function Details() {
   
   return(
-    <>
+    <Container>
     <h1>koe koe</h1>
     <samp>menó</samp>
-    </>
+    </Container>
   )
 }
---
+++ Rocketnotes/src/pages/Details/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: rgb(126, 224, 195);
+`
+
+
+
+// body{
+//   
+// }
