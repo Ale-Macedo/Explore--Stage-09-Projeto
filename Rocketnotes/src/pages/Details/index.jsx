d62625965369f27413e3f2ee7e242f9656f94997
Alessandro Macedo <aleluiz73@gmail.com>
2022-09-02 19:31:20 -0300

    criando componente header

--- Rocketnotes/src/pages/Details/index.jsx
+++ Rocketnotes/src/pages/Details/index.jsx
@@ -1,17 +1,15 @@
 import { Container } from './styles';
 
 import { Button } from '../../components/Button';
+import { Header } from '../../components/Header'; 
 
 export function Details() {
   
   return(
-    <Container>
-    <h1>koe koe</h1>
-    <samp>menó</samp>
-
-    <Button title="Aperta o meu botão" loading  />
-    <Button title="koe o meu bortao não" />
-    <Button title="koe koe" />
+    <Container>  
+      <Header />
+      
+      <Button title="Voltar" />
     </Container>
   )
 }
--- Rocketnotes/src/pages/Details/styles.js
+++ Rocketnotes/src/pages/Details/styles.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  /* background: rgb(126, 224, 195); */
+  
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+
+  grid-template-rows: 105px auto;
+  grid-template-areas: 
+  "header"
+  "content"
+  ;
+
+
 `
-
-
-
-// body{
-//   
-// }
